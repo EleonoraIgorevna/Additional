42e9b967e0066d68851e43ed50b14927baa25069
[EleonoraPogorelova] <[pogorelova.eva@bk.ru]>
2021-04-19 13:11:35 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,457 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tatic void Main(string[] args)
+            {/*
+            //Метанит Теория 
+            //УМНОЖЕНИЕ КАЖДОГО ИНДЕКСА МАССИВА НА 2.
+            int[] max = new[] { 1, 2, 4, 5, 7 }; // одномерный массив.
+            for (int i = 0; i < max.Length; i++)
+            {
+                max[i] = max[i] * 2;
+                Console.WriteLine(max[i]);
+            }  //постепенно умножение каждого индекса массива на 2;
+
+
+
+
+
+
+            //ПОСТРОЕНИЕ МАССИВА В ОБРАТНОМ ПОРЯДКЕ.
+            char [] hello = new[] { 'h', 'e', 'l', 'l', 'o' };
+            {
+                for (int i = hello.Length - 1; i <= hello.Length - 1; i--) //Инверсия массива
+                {
+                    Console.WriteLine($"{ hello [i]} ") ;
+                    Console.ReadLine();                }
+                Console.ReadLine();
+          
+
+
+                //ПОСТРОНИЕ
+                int[,] mas = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+                int rows = mas.GetUpperBound(0) + 1;
+                int columns = mas.Length / rows;
+                // или так
+                // int columns = mas.GetUpperBound(1) + 1;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        Console.Write($"{mas[i, j]} \t");
+                    }
+                    Console.WriteLine();}
+
+            
+
+
+
+
+
+             // ПЕРЕБИРАЕМ МАССИВ МАССИВОВ 
+            int[][] value = new int[3][];
+            value[0] = new int[] { 1, 2 };
+            value[1] = new int[] { 3, 4, 3 };
+            value[2] = new int[] { 7, 8, 5, 9 };
+
+
+
+
+
+
+
+
+            // НАЙДЕМ КОЛИЧЕСТВО ПОЛОЖИТЕЛЬНЫХ ЦИФР В МАССИВЕ
+            int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine($"Число элементов больше нуля: {result}");
+
+
+
+
+
+
+
+            // НАЙДЕМ КОЛИЧЕСТВО ОТРИЦАТЕЛЬНЫХ ЦИФР В МАССИВЕ
+            int[] body = new int [] { 1,2,3,4,5,6,-1,-2,-8 };
+            int result01 = 0;
+            foreach (int X in body)
+            {
+                if (X < 0)
+                {
+                    result01++;
+                }          
+             }
+            Console.WriteLine( $" Число значений меньше нуля:   { result01}" ) ;
+            
+
+
+
+
+            //ПЕРЕБИРАЮ ОДНОМЕРНЫЙ МАССИВ
+            int[] massiv1 = { 1,2,3,4,5,6,7};
+            for (int i=0; i < massiv1.Length; i++)
+            { 
+                Console.Write($"{massiv1[i]} " );           
+            }
+           
+
+
+
+
+
+            //ПЕРЕБИРАЮ ДВУМЕРНЫЙ МАССИВ
+            int[,] massiv1 = [ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }];
+            for (int i = 0; i < massiv1.GetLength; i++)
+            {
+                for (int j = 0; j < massiv1.Length ; j++)
+                {
+                    Console.Write($"{massiv1[i, j]} \t");
+                }
+                Console.WriteLine();
+            }         
+            // ВЫВОД МАССИВА С КОНСОЛИ
+            Console.Write("Введите число эллементов масива: \t");
+            int elements = int.Parse(Console.ReadLine());
+            int[] array = new int[elements];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"\n Введите элемент массива под индексом {i}: \t ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+                Console.Write($" Вывод массива: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+
+
+
+            // ВЫВОд МАссива с консоли в обратном порядке
+            Console.Write("Введите число эллементов масива: \t");
+            int elements = int.Parse(Console.ReadLine());
+            int[] array = new int[elements];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"\n Введите элемент массива под индексом {i}: \t ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write($" Вывод массива: ");
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+            }
+
+         
+
+
+            // НАЙТИ СУММУ ЧЕТНЫХ ЧИСЕЛ МАССИВА
+            int[] massiv2 = { 1, 2, 3,5,8 };
+            int result = 0;
+            for (int i = 0; i < massiv2.Length; i++)
+            {
+                if (massiv2[i] % 2 == 0)
+                {
+                    result += massiv2[i];
+                }
+            }
+            Console.Write(result);
+
+
+
+
+
+           // НАЙТИ СУММУ ЧЕТНЫХ ЧИСЕЛ МАССИВА C ИСПОЛЬЗОВАНИЕМ ТЕРМАЛЬНОГО ОПЕРАТОРА.
+            int[] massiv2 = { 1, 2, 3, 5, 8 };
+            int result = 0;
+            for (int i = 0; i < massiv2.Length; i++)
+            {
+                int x= (massiv2[i] % 2 == 0) ? result += massiv2[i] : 0;
+            }
+            Console.Write(result);
+            
+             */
+                // Найти наибольший элемент в массиве.
+
+                /// ВОПРОССССС-  Как через ЦИКЛ найти максимальное и минимальное значение массива??
+                /*
+                int[] myarray = { 1, 0, -2, 9, 6, 4, 0, 3, 1, 15 };
+
+                Console.WriteLine(myarray.Max());
+                Console.WriteLine(myarray.Min());
+                Console.WriteLine(myarray.Sum());
+                int[] result = myarray.Distinct().ToArray();
+
+                Console.WriteLine(Array.Sort(myarray));
+
+
+
+
+
+                /*Немного теории для себя 
+                           Console.WriteLine("One");
+                           int [] mix= { 5, 2, 3 };
+                           int [,] mix1= new int [4, 5]; //двумерный массив , матрица .
+                           int [,,] mix2= new int  [3, 5, 4]; //трехмерный массив, матрица, массив массивов.
+
+                           Console.WriteLine(mix [1]);
+                           //Для перебора элементов массива используют специальные операторы- циклы. 
+                           Console.WriteLine("Two");
+
+                           int[] array01 = { 2, 7, 6, 9, 4, 6 };
+
+                           for (int f = 0; f < 6; f++)
+                           {
+
+                               Console.WriteLine(array01[f]);
+
+
+                           }
+
+                           Console.WriteLine("Three");
+                           int[] array02 = { 2, 7, 6, 9, 4 };
+
+                           for (int l = 0; l < array02.Length ; l++)
+                           {
+
+                               Console.WriteLine(array02[l]);
+
+
+                           }
+
+
+
+                           Console.WriteLine("Four");
+
+                           int[][] a = new int[3][];
+                           a[0] = new int[8];
+                           a[1] = new int[4];
+                           a[2] = new int[3];
+                           int a00 = a[0][0];
+
+                           a[2][3] = 1;
+
+                           Console.WriteLine(a[0]);
+
+
+
+
+
+
+                           Console.WriteLine("Five");
+
+
+                           int[,] zero = new int[5, 5];
+                           zero[2, 2] = 1;
+
+                           for (int i = 0; i < zero.GetLength(0); i++)
+                           {
+
+                               for (int j=0; j< zero.GetLength(1); j++) {
+
+
+                                   Console.Write($"{zero[i,j]}");
+
+
+                               }
+                               System.Console.WriteLine();
+
+                           }
+
+                           int[,] matrix = new int[5, 5];
+
+                           matrix[2, 3] = 1;
+                           matrix[2, 4 ] = 1;
+
+                           for (int i = 0; i < matrix.GetLength(0); i++)
+                           {
+                               for (int j = 0; j < matrix.GetLength(1); j++)
+                               {
+                                   System.Console.Write($"{matrix[i, j]} ");
+                               }
+                               System.Console.WriteLine();
+                           }
+
+
+
+                           while (true)
+                           {
+                               double input = Convert.ToDouble(Console.ReadLine());
+
+
+                               {
+
+                                   if (input > 0)
+                                   { Console.WriteLine(input); }
+                                   else if (input == 0)
+                                   { Console.WriteLine("Нуль"); }
+                                   break;
+
+                               }
+
+                               /*
+
+                           string password;
+                           do
+                           {
+                               Console.WriteLine("Задайте пароль длиной не менее 5 знаков");
+                               password = Console.ReadLine();
+                           } while (password.Length < 5);
+
+                           string repeatedPassword;
+                           do
+                           {
+                               Console.WriteLine("Повторите пароль:");
+                               repeatedPassword = Console.ReadLine();
+                           } while (repeatedPassword != password);
+
+                           Console.WriteLine("Пароль успешно установлен");
+
+
+
+
+                           string[] students =
+                                        {
+                                           "Иванов",
+                                           "Петров",
+                                           "Сидоров",
+                                           "Петрова",
+                                           "Филиппова",
+                                           "Егоров",
+                                           "Козлова",
+                                       };
+
+                           Console.Write("Введите фамилию студента:");
+                           string lastName = Console.ReadLine();
+                           bool isFound = false;
+
+                           for (int i = 0; i < students.Length; i++)
+                           {
+                               if (students[i] == lastName)
+                               {
+                                   Console.WriteLine($"Студент с фамилией {lastName} находится под номером {i + 1}");
+                                   isFound = true;
+                                   break;
+                               }
+                           }
+
+                           if (!isFound)
+                           {
+                               Console.WriteLine("Такого студента нет в списке :(");
+                           }
+
+
+
+
+                           ВЫВЕДИ НЕЧЕТНЫЕ ЧИСЛА из 800.
+
+                           for (int a = 0; a < 10; a++) 
+
+                           {
+                               if (a%2==0)
+                               {
+                                   continue;
+                               }
+                               Console.WriteLine(a);
+                           }
+
+
+                           char[] asd = { 'w', 'o', 'r', 'l', 'd' };
+
+                           for (int i = 0; i < asd.Length; i++)
+                           {
+                               Console.Write(asd[i]);
+
+                           }
+
+               //tring[] asd01 { "world" };
+
+
+               char[] asd = { 'w', 'o', 'r', 'l', 'd' };
+
+               for (int i = 0; i < asd.Length; i++)
+               {
+                   Console.Write(asd[i]);
+
+               }
+
+
+
+               // Первое
+
+                   string greetings = "Hello";
+
+                   for (int i = greetings.Length -1  ; i >= 0; i--)
+                   {
+                       Console.Write(greetings[i]);
+                   }
+
+                   Console.Read();
+
+
+               //Второе
+
+               string [,] directory = new string [,]
+
+               {
+
+               { "Иванов A.A" ,      "+7915872345 / Ivanov@bk.ru" },
+               { "Сидоров К.М",      "+7915862457 / Sidorov@mail.ru" },
+               { "Быков А.Ю.  ",     "+7919532655 / Bikov@bk.ru" },
+               { "Филимонова С.С.",  "+7985696328 / Filimonova@mail.ru" },
+               { "Юрченко К.И.",     "+7936152847 / Yurshenko@mail.ru"},
+
+
+               };
+
+               for (int i = 0; i < directory.GetLength(0); i++)
+               {
+                   for (int j = 0; j < directory.GetLength(1); j++)
+                   {
+                       Console.Write(directory[i, j]);
+                   }
+
+                   System.Console.ReadLine();
+
+                   */
+
+                /*
+                int[,] myArray = new int[,]
+                {
+                    {2, 45, 12, 51, 51},
+                    {6, 7, 8, 3, 1},
+                    {1, 5, 65, 78, 13},
+                    {6, 3, 151, 5, 65},
+                    {64, 43, 15, 5, 37}
+                };
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.WriteLine(myArray[i, i]);
+                }
+
+
+
+                int[] array = { 1, 2, 3, 4, 5 };
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(array[i]);
+                }
+
+                */
+            }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             //Запрашиваем минимальную погоду за сутки.
+            Console.WriteLine("Пожалуйста введи минимальную погоду за сутки.");
+            string MinTemperature = Console.ReadLine();
+            //Вводим три переменные, где а - манимальная температура, b - максимальная температура за сутки, result - средняя температура за сутки.
+            double a, b, result;
+            //Конвертируем из строкового значение Минимальной температуры  в числовое значение с плавающей точкой.
+            a = Convert.ToByte(MinTemperature);
+            //Вывводим результат (минимальная температура) на консоль.
+            Console.WriteLine("Минимальная температура " + a + " градусов");
+            //Запрашиваем максимальную  погоду за сутки.
+            Console.WriteLine("Пожалуйста введи  максимальную погоду за сутки.");
+            string MaxTemperature = Console.ReadLine();
+            //Конвертируем из строкового значение Максимальной температуры  в числовое значение с плавающей точкой.
+            b = Convert.ToByte(MaxTemperature);
+            //Вывводим результат (максимальная температура) на консоль.
+            Console.WriteLine("Максимальная температура " + b + " градусов");
+            //Просчитываем среднее значение минимальной и максимальной температуры за сутки.
+            result = ((a + b) / 2);
+            //Выводим результат на консоль.
+            Console.WriteLine("Средняя температура за сутки  " + result + " градусов");
+            */
+
+
+            /*
+             * 
+              double Count = 500.34;
+            int quantity = 1;
+
+
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(" ________________________________________");
+            Console.WriteLine("|              OOO'Ромашка'              |");
+            Console.WriteLine("|             ИНН:  45632455             |");
+            Console.WriteLine("|  Россия, г.Москва, ул. Профсоюзная 42  |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|          КАССОВЫЙ ЧЕК/ПРИХОД           |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("| Доставка цветов                 " + quantity + " шт   |");
+            Console.WriteLine("| Услуга   " + Count + "                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                      " + DateTime.Now + "|");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|               @}-->--                  |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|                                        |");
+            Console.WriteLine("|_________СПАСИБО ЗА ПОКУПКУ!____________|");
+
+
+
+
+
+
+
+
+
+
+
+                        //Запрашивает число.
+                        Console.WriteLine("Пожалуйста введи любое число. Мы проверим его на четность.");
+                        double value = Convert.ToDouble(Console.ReadLine());
+                        //Вводим переменную, которая через % будет проверять четность введенного ранее числа. 
+                        double result = value % 2;
+                        //Прописываем условия четности. 
+                        if (result > 0) { Console.WriteLine("Число не четное."); }
+                        if (result == 0) { Console.WriteLine("Число четное."); }
+
+
+
+
+            //Запрашиваем число месяца.
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Пожалуйста, введите любое число от 1 до 12. Программа выведет на экран название месяца, которое соответствует введенному числу.");
+            //Создаем переменную а и присваеваем ей целое значение.
+            int a = Convert.ToInt32(Console.ReadLine());
+            int MonthOfYear = a;
+            string MonthTitle = "";
+
+            //Прописываем номера и их соотвествия месяцам.
+            switch (MonthOfYear)
+            {
+                case 1:
+                    MonthTitle = "Январь";
+                    break;
+                case 2:
+                    MonthTitle = "Февраль";
+                    break;
+                case 3:
+                    MonthTitle = "Март";
+                    break;
+                case 4:
+                    MonthTitle = "Апрель";
+                    break;
+                case 5:
+                    MonthTitle = "Май";
+                    break;
+                case 6:
+                    MonthTitle = "Июнь";
+                    break;
+                case 7:
+                    MonthTitle = "Июль";
+                    break;
+                case 8:
+                    MonthTitle = "Август";
+                    break;
+                case 9:
+                    MonthTitle = "Сентябрь";
+                    break;
+                case 10:
+                    MonthTitle = "Октябрь";
+                    break;
+                case 11:
+                    MonthTitle = "Ноябрь";
+                    break;
+                case 12:
+                    MonthTitle = "Декабрь";
+                    break;
+                default:
+                    Console.WriteLine("Введите корректное число.");
+                    break;
+            }
+            Console.WriteLine(MonthTitle);
+
+
+
+
+            //Запрашиваем число месяца.
+            Console.WriteLine("Пожалуйста, введите любое число от 1 до 12. Программа выведет на экран название месяца, которое соответствует введенному числу.");
+            //Создаем переменную а и присваеваем ей целое значение.
+            int a = Convert.ToInt32(Console.ReadLine());
+            int MonthOfYear = a;
+            string MonthTitle = "";
+
+            //Прописываем номера и их соотвествия месяцам.
+            switch (MonthOfYear)
+            {
+                case 1:
+                    MonthTitle = "Январь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 2:
+                    MonthTitle = "Февраль";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 3:
+                    MonthTitle = "Март";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 4:
+                    MonthTitle = "Апрель";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 5:
+                    MonthTitle = "Май";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 6:
+                    MonthTitle = "Июнь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 7:
+                    MonthTitle = "Июль";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 8:
+                    MonthTitle = "Август";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 9:
+                    MonthTitle = "Сентябрь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 10:
+                    MonthTitle = "Октябрь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 11:
+                    MonthTitle = "Ноябрь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                case 12:
+                    MonthTitle = "Декабрь";
+                    Console.WriteLine(MonthTitle);
+                    break;
+                default:
+                    Console.WriteLine("Введите корректное число.");
+                    break;
+
+
+
+             public enum Months 
+        { 
+            January = 1,
+            Febuary,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+        static void Main(string[] args)
+        {/* Не получается данный вариант. Хочу понять в чем причина
+            int value = Convert.ToInt32(Console.ReadLine((value)));
+            Console.WriteLine(value); */
+        }
+
+
+                        */
+
+
+
+
+
+
+
+
+
+            ;
+        }
+}
+}
